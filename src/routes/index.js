a38f73df478e198e0bce42e8296681f1d7e0e0ab
Carlos E <echeverriadev@gmail.com>
2022-03-26 21:08:47 -0500

    [init] Implementing swagger to doc the API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const router = Router();
 
 const userRoute = require("./user.route");
 
 router.use('/users', userRoute);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 router.get('/', (req, res) => {
   res.send("HELLO WORLD")
 })
